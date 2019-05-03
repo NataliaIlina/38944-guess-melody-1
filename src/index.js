13f187accf6db651d6606604197dee998013819a
NataliaIlina <metlinova86@gmail.com>
2019-05-03 11:27:42 +0300

    Удаляет неиспользуемые пропсы

--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 
 const init = () => {
-  ReactDOM.render(
-      <App time={10} errorsCount={4} />,
-      document.querySelector(`.main`)
-  );
+  ReactDOM.render(<App />, document.querySelector(`.main`));
 };
 
 init();
